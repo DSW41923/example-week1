b53cef05594e9054e4372e246239a02feac53c35
DSW41923 <dsw41923@gmail.com>
2014-05-02 22:53:48 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,24 +1,25 @@
 /**
  * @overview
  *
- * @author
- * @version 2014/04/26
+ * @author DSW
+ * @version 2014/05/02
  */
 
 var http = require("http");
 var port = 1337;
 var request = require("request");
-var url = "http://graph.facebook.com/Boo/photos?type=uploaded";
+var target = prompt("Please Enter the Name of Target Page(for example: HornyDragonTW):");
+var url = "http://graph.facebook.com/" + target + "/photos?type=uploaded";
 
 http.createServer(function (req, res) {
   res.writeHeader(200, {"Content-Type": "text/html"});
   
-  var data = "<html><head><title>hello world</title></head><body>"
+  var data = "<html><head><style>div{background-color:#3366FF;}h1{text-align:center;color:#FFFFFF;}</style><title>FB PHOTO GET TESTING</title></head><body><div><h1>Here are Photos</h1></div>"
   request.get(url, function (err, body, response) {
 
     response = JSON.parse(response);
     response.data.forEach(function (val, idx) {
-      data += "<img src='" + val.images[2].source + "'>";
+      data += "<img src='" + val.images[2].source + "'width="300" height="300"" + "'>";
     });
     
     data += "</body></html>";
