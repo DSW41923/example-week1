a1ce6e36089acbdabd4c9d4298e1eb1313619078
DSW41923 <dsw41923@gmail.com>
2014-05-03 18:29:32 +0800

    Update for Week 2

--- app.js
+++ app.js
@@ -8,25 +8,33 @@
 var http = require("http");
 var port = 1337;
 var request = require("request");
+var fs =require("fs")
 var url = "http://graph.facebook.com/HornyDragonTW/photos?type=uploaded";
+var template = "";
+
+/*fs.readFile("./index.html", "UTF-8", function (err, result) {
+            template = result;
+            });*/
+
+//same as/*~*/
+template = fs.readFileSync("./index.html", "UTF-8");
 
 http.createServer(function (req, res) {
   res.writeHeader(200, {"Content-Type": "text/html"});
   
-  var data = "<html><head><style>div{background-color:#3366FF;}h1{text-align:center;color:#FFFFFF;}body{background: #A8E5E5;}</style><title>FB PHOTO GET TESTING</title></head><body><div><h1>Here are Photos</h1></div>"
+  var data = "";
   request.get(url, function (err, body, response) {
 
     response = JSON.parse(response);
     response.data.forEach(function (val, idx) {
-      data += "<img src='" + val.images[2].source + "'width=400 height=400>";
+      data += "<img src='" + val.images[2].source + "'width=400 height=400 / >";
     });
     
-    data += "</body></html>";
+    data = template.replace("{{context}}", data);
     res.end(data);
   });
 
 }).listen(port);
 
 console.log("start server port: " + port);
-
 
