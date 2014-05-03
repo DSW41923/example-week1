954549a5e28dc440be618302cdd0f8c61cc71056
DSW41923 <dsw41923@gmail.com>
2014-05-03 10:22:59 +0000

    Update of Week 2

--- app.js
+++ app.js
@@ -8,21 +8,29 @@
 var http = require("http");
 var port = 1337;
 var request = require("request");
-var target = prompt("Please Enter the Name of Target Page(for example: HornyDragonTW):");
-var url = "http://graph.facebook.com/" + target + "/photos?type=uploaded";
+var fs =require("fs")
+var url = "http://graph.facebook.com/HornyDragonTW/photos?type=uploaded";
+var template = "";
+
+/*fs.readFile("./index.html", "UTF-8", function (err, result) {
+            template = result;
+            });*/
+
+//same as/*~*/
+template = fs.readFileSync("./index.html", "UTF-8");
 
 http.createServer(function (req, res) {
   res.writeHeader(200, {"Content-Type": "text/html"});
   
-  var data = "<html><head><style>div{background-color:#3366FF;}h1{text-align:center;color:#FFFFFF;}</style><title>FB PHOTO GET TESTING</title></head><body><div><h1>Here are Photos</h1></div>"
+  var data = "";
   request.get(url, function (err, body, response) {
 
     response = JSON.parse(response);
     response.data.forEach(function (val, idx) {
-      data += "<img src='" + val.images[2].source + "'width="400" height="400"" + "'>";
+      data += "<img src='" + val.images[2].source + "'width=400 height=400 / >";
     });
     
-    data += "</body></html>";
+    data = template.replace("{{context}}", data);
     res.end(data);
   });
 
